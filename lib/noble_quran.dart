15b72a9742b4cc646f91aaa8f0fc336640f86a3d
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2022-08-24 09:09:29 +0300

    config: added new methods to help with translations

--- lib/noble_quran.dart
+++ lib/noble_quran.dart
@@ -30,9 +30,10 @@ class NobleQuran {
   ///  Parameter: None
   ///  Returns: List of SurahTitle
   static Future<List<NQSurahTitle>> getSurahList() async {
-    String surahListJsonStr = await rootBundle.loadString(NQPathConstants.surahListPath);
-    List<NQSurahTitle> surahFromJson =
-        List<NQSurahTitle>.from(json.decode(surahListJsonStr).map((x) => NQSurahTitle.fromJson(x)));
+    String surahListJsonStr =
+        await rootBundle.loadString(NQPathConstants.surahListPath);
+    List<NQSurahTitle> surahFromJson = List<NQSurahTitle>.from(
+        json.decode(surahListJsonStr).map((x) => NQSurahTitle.fromJson(x)));
     return surahFromJson;
   }
 
@@ -40,9 +41,10 @@ class NobleQuran {
   ///  Parameter: index as int of the required surah
   ///  Returns: SurahTitle containing the title and details of the Surah
   static Future<NQSurahTitle> getSurahTitleAtIndex(int index) async {
-    String surahListJsonStr = await rootBundle.loadString(NQPathConstants.surahListPath);
-    List<NQSurahTitle> surahFromJson =
-        List<NQSurahTitle>.from(json.decode(surahListJsonStr).map((x) => NQSurahTitle.fromJson(x)));
+    String surahListJsonStr =
+        await rootBundle.loadString(NQPathConstants.surahListPath);
+    List<NQSurahTitle> surahFromJson = List<NQSurahTitle>.from(
+        json.decode(surahListJsonStr).map((x) => NQSurahTitle.fromJson(x)));
     return surahFromJson[index];
   }
 
@@ -50,8 +52,8 @@ class NobleQuran {
   ///  Parameter: index as int of the required surah
   ///  Returns: List of Ayats with each ayat containing a List of NQWord
   static Future<List<List<NQWord>>> getSurahWordByWord(int surahIndex) async {
-    String wordByWord =
-        await rootBundle.loadString('${NQPathConstants.quranPath}/$surahIndex.json');
+    String wordByWord = await rootBundle
+        .loadString('${NQPathConstants.quranPath}/$surahIndex.json');
     return nqWordFromJson(wordByWord);
   }
 
@@ -59,7 +61,8 @@ class NobleQuran {
   ///  Parameter: index as int of the required surah
   ///  Returns: NQSurah containing the arabic ayats
   static Future<NQSurah> getSurahArabic(int surahIndex) async {
-    String quranArabicJson = await rootBundle.loadString(NQPathConstants.quranArabicPath);
+    String quranArabicJson =
+        await rootBundle.loadString(NQPathConstants.quranArabicPath);
     Map<String, dynamic> jsonDict = json.decode(quranArabicJson);
     return NQSurah.fromJson(jsonDict["quran"]["sura"][surahIndex]);
   }
@@ -68,7 +71,8 @@ class NobleQuran {
   ///  Parameter: index as int of the required surah
   ///  Returns: NQSurah containing the arabic ayats
   static Future<NQSurah> getSurahTransliteration(int surahIndex) async {
-    String quranArabicJson = await rootBundle.loadString(NQPathConstants.quranTransliterationPath);
+    String quranArabicJson =
+        await rootBundle.loadString(NQPathConstants.quranTransliterationPath);
     Map<String, dynamic> jsonDict = json.decode(quranArabicJson);
     return NQSurah.fromJson(jsonDict["quran"]["sura"][surahIndex]);
   }
@@ -78,10 +82,63 @@ class NobleQuran {
   ///     index as int of the required surah
   ///     translation as NQTranslation
   ///  Returns: NQSurah containing the translated ayats
-  static Future<NQSurah> getTranslationString(int surahIndex, NQTranslation translation) async {
-    String translationJsonStr = await rootBundle
-        .loadString('${NQPathConstants.quranTranslationPath}/${translation.rawValue}.json');
-    Map<String, dynamic> fullQuranTranslationMap = json.decode(translationJsonStr);
-    return NQSurah.fromJson(fullQuranTranslationMap["quran"]["sura"][surahIndex]);
+  static Future<NQSurah> getTranslationString(
+      int surahIndex, NQTranslation translation) async {
+    String translationJsonStr = await rootBundle.loadString(
+        '${NQPathConstants.quranTranslationPath}/${translation.rawValue}.json');
+    Map<String, dynamic> fullQuranTranslationMap =
+        json.decode(translationJsonStr);
+    return NQSurah.fromJson(
+        fullQuranTranslationMap["quran"]["sura"][surahIndex]);
   }
+
+  /// Returns a list of all translations available
+  static List<NQTranslation> getAllTranslations() {
+    return [
+      NQTranslation.clear,
+      NQTranslation.sahih,
+      NQTranslation.hilali,
+      NQTranslation.malayalam_abdulhameed,
+      NQTranslation.malayalam_karakunnu,
+      NQTranslation.urdu_maududi
+    ];
+  }
+
+  /// Returns a translation enum from its title string
+  static NQTranslation getTranslationFromTitle(String title) {
+    if (title == NQTranslation.clear.title) {
+      return NQTranslation.clear;
+    } else if (title == NQTranslation.sahih.title) {
+      return NQTranslation.sahih;
+    } else if (title == NQTranslation.hilali.title) {
+      return NQTranslation.hilali;
+    } else if (title == NQTranslation.malayalam_abdulhameed.title) {
+      return NQTranslation.malayalam_abdulhameed;
+    } else if (title == NQTranslation.malayalam_karakunnu.title) {
+      return NQTranslation.malayalam_karakunnu;
+    } else if (title == NQTranslation.urdu_maududi.title) {
+      return NQTranslation.urdu_maududi;
+    }
+    return NQTranslation.clear;
+  }
+
+  /// Returns a translation enum from its raw value string
+  static NQTranslation getTranslationFromValue(String value) {
+    if (value == NQTranslation.clear.rawValue) {
+      return NQTranslation.clear;
+    } else if (value == NQTranslation.sahih.rawValue) {
+      return NQTranslation.sahih;
+    } else if (value == NQTranslation.hilali.rawValue) {
+      return NQTranslation.hilali;
+    } else if (value == NQTranslation.malayalam_abdulhameed.rawValue) {
+      return NQTranslation.malayalam_abdulhameed;
+    } else if (value == NQTranslation.malayalam_karakunnu.rawValue) {
+      return NQTranslation.malayalam_karakunnu;
+    } else if (value == NQTranslation.urdu_maududi.rawValue) {
+      return NQTranslation.urdu_maududi;
+    }
+    return NQTranslation.clear;
+  }
+
+
 }
